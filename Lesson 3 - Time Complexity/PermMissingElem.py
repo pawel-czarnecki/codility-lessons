055c7706bea9d04786af5f3915bdf874992d102d
Paweł Czarnecki <pawel.r.czarnecki@gmail.com>
2017-03-06 23:32:39 +0100

    Added lesson 3

---
+++ Lesson 3 - Time Complexity/PermMissingElem.py
@@ -0,0 +1,46 @@
+'''
+A zero-indexed array A consisting of N different integers is given. The array contains integers
+in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+    def solution(A)
+
+that, given a zero-indexed array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+    A[0] = 2
+    A[1] = 3
+    A[2] = 1
+    A[3] = 5
+
+the function should return 4, as it is the missing element.
+
+Assume that:
+
+    - N is an integer within the range [0..100,000];
+    - the elements of A are all distinct;
+    - each element of array A is an integer within the range [1..(N + 1)].
+
+Complexity:
+
+    - expected worst-case time complexity is O(N);
+    - expected worst-case space complexity is O(1), beyond input storage
+      (not counting the storage required for input arguments).
+'''
+
+'''
+    Analysis URL: https://codility.com/demo/results/trainingV5JZKH-DCX/
+    Correctness:  100%
+    Performance:  100%
+    Task score:	  100%
+'''
+
+def solution(A):
+    array_sum = sum(A)
+    elements_sum = sum(int(i) for i in range(1, len(A) + 2))
+        
+    return elements_sum - array_sum
